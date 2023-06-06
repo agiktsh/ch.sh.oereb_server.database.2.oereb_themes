ff0a01c48fea60f7cd2e6d457d13e5166ae84d11
Clemens Rudert <crud3_rt@posteo.ch>
2023-06-06 16:34:01 +0200

    fix setup

---
+++ scripts/insert_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+ili2pg_executable=$(find /tmp/ili2pg -iname 'ili2pg*.jar')
+files=$(find . -type f -name "*.xtf")
+echo "$files"
+for file in $files; do
+  java -jar "$ili2pg_executable" \
+    --import \
+    --replace \
+    --dbdatabase "$POSTGRES_DB" \
+    --dbusr gretl \
+    --dbport 54321 \
+    --dbhost localhost \
+    --dbpwd "$PG_GRETL_PWD" \
+    --dbschema "$SCHEMA" \
+    --dataset "$(basename "$file" .xtf)" \
+    --disableValidation \
+    --models "OeREBKRMtrsfr_V2_0" \
+    --verbose \
+    --modeldir http://models.interlis.ch/ \
+    "$file"
+done
