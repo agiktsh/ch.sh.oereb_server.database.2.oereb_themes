9b186c6b2edd686b06ef87a193b2f7690e5a0605
romefi <romedi.filli@sh.ch>
2023-06-14 12:27:02 +0200

    Update insert_all.sh
    Nur rasch testen, ob Datenimport überhaupt funktioniert. Änderung mache ich gleich wieder rückgängig. Und kein Commit, da Clemens in den Ferien ist.

--- scripts/insert_all.sh
+++ scripts/insert_all.sh
@@ -3,16 +3,16 @@ set -Eeuo pipefail
 
 ili2pg_executable=$(find /tmp/ili2pg -iname 'ili2pg*.jar')
 # Das wird später benutzt damit alle Dateien integriert werden
-files=$(find . -type f -name "*.xtf")
+# files=$(find . -type f -name "*.xtf")
 # uncomment this to directly try a file
-# files=(
+ files=(
 # "ch.Gewaesserraum-Gewaesserraum_V1_1-ch.SH.Gewaesserraum.xtf" \
-# "ch.Laermempfindlichkeitsstufen-Laermempfindlichkeitsstufen_V1_2-ch.2911.Laermempfindlichkeitsstufen.xtf" \
+"ch.Laermempfindlichkeitsstufen-Laermempfindlichkeitsstufen_V1_2-ch.2939.Laermempfindlichkeitsstufen.OeREBKRMtrsfr_V2_0.xtf" \
 # "ch.Laermempfindlichkeitsstufen-Laermempfindlichkeitsstufen_V1_2-ch.2951.Laermempfindlichkeitsstufen.xtf" \
 # "ch.Nutzungsplanung-Nutzungsplanung_V1_2-ch.2920.Nutzungsplanung.xtf" \
 # "ch.Nutzungsplanung-Nutzungsplanung_V1_2-ch.2939.Nutzungsplanung.xtf" \
 # "ch.Planungszonen-Planungszonen_V1_1-ch.SH.Planungszonen.xtf" \
-# )
+ )
 
 echo "${files[*]}"
 for file in ${files[*]}; do
